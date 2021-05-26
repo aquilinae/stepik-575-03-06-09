5de1f465aa4489589f82b44d5b0c556d11d6bdff
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-26 23:50:56 +0300

    Add option for choosing browser between Chrome or Firefox via CLI

--- conftest.py
+++ conftest.py
@@ -1,9 +1,13 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 def pytest_addoption(parser):
+    parser.addoption(
+        '--browser', action='store', default='chrome', help='Launch specific browser (possible Chrome or Firefox)'
+    )
     parser.addoption(
         '--language', action='store', default='en-gb', help='Choose shop language'
     )
@@ -11,9 +15,18 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='function')
 def driver(request):
-    __chrome_options = ChromeOptions()
-    __chrome_options.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption('language')})
-    driver = webdriver.Chrome(options=__chrome_options)
+    _browser = request.config.getoption('browser').lower()
+    _language = request.config.getoption('language')
+    if _browser == 'chrome':
+        __chrome_options = ChromeOptions()
+        __chrome_options.add_experimental_option('prefs', {'intl.accept_languages': _language})
+        driver = webdriver.Chrome(options=__chrome_options)
+    elif _browser == 'firefox':
+        __firefox_options = FirefoxOptions()
+        __firefox_options.set_preference('intl.accept_languages', _language)
+        driver = webdriver.Firefox(options=__firefox_options)
+    else:
+        raise pytest.UsageError('Browser should be Chrome (default) or Firefox')
 
     yield driver
 
