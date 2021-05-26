60b4f607a012cead933a3603d3886d09f8c7dc1e
ornichola <1502708+ornichola@users.noreply.github.com>
2021-05-26 23:48:37 +0300

    Add option for setting language via CLI

--- conftest.py
+++ conftest.py
@@ -1,10 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--language', action='store', default='en-gb', help='Choose shop language'
+    )
 
 
 @pytest.fixture(scope='function')
-def driver():
-    driver = webdriver.Chrome()
+def driver(request):
+    __chrome_options = ChromeOptions()
+    __chrome_options.add_experimental_option('prefs', {'intl.accept_languages': request.config.getoption('language')})
+    driver = webdriver.Chrome(options=__chrome_options)
 
     yield driver
 
